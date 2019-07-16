42b31c86810293c385b3f38dec51f846ad9c7cc4
Episkey <paul.beziat66@gmail.com>
2019-07-16 16:05:13 +0200

    UserController

--- src/main/java/com/wcs/FlashCard/Controller/UserController.java
+++ src/main/java/com/wcs/FlashCard/Controller/UserController.java
@@ -1,4 +1,35 @@
 package com.wcs.FlashCard.Controller;
 
+import com.wcs.FlashCard.Model.User;
+import com.wcs.FlashCard.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/user/{userId}")
+    public User read(@PathVariable Long userId) {
+        return userRepository.findById(userId).get();
+    }
+
+    @PostMapping("/user")
+    public User create(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/user/{userId}")
+    public User update(@PathVariable Long userId, @RequestBody User user) {
+        User userToUpdate = userRepository.findById(userId).get();
+        if (user.getPassword() != null) {
+            userToUpdate.setPassword(user.getPassword());
+        }
+        if (user.getMail() != null) {
+            userToUpdate.setMail(user.getMail());
+        }
+        return userRepository.save(userToUpdate);
+    }
 }
